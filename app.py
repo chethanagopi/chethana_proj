7b5a90596b72d457600838249004dfebeab2885b
chethana <chethanagopi123@gmail.com>
2024-07-30 13:18:23 +0530

    first_commit

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask,request
+
+app=Flask(__name__)
+
+stores=[
+    {
+        "name":"my_store",
+        "items":[
+            {
+                "name":"chair",
+                "price":15.99
+            }
+        ]
+    }
+]
+
+@app.get('/store')
+def get_store():
+    return {"stores":stores}
+
+@app.post('/store')
+def create_store():
+    request_data=request.get_json()
+    new_store = {"name":request_data["name"],"items":[]}
+    stores.append(new_store)
+    return new_store, 201
+
+@app.post('/store/<string:name>/item')
+def create_item(name):
+    request_data=request.get_json()
+    for store in stores:
+      if store["name"] == name:
+          new_item={"name":request_data['name'],"price":request_data['price']}
+          store["items"].append(new_item)
+          return new_item, 201
+    return {"message":"no data"},404
+
+@app.get('/store/<string:name>')
+def get_name(name):
+    for store in stores:
+        if store['name']==name:
+            return store
+    return {'message':"not data"}, 404
+
+@app.get('/store/<string:name>/item')
+def get_items(name):
+    for store in stores:
+        if store['name']==name:
+            get_items=store['items']
+            return get_items
+    return {'message':"not data"}, 404
